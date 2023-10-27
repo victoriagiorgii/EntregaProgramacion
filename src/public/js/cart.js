d3cf972e579a3eb8aaf780916d4cb985d0c6bcf0
victoriaagiorgi <victorianovillo09@gmail.com>
2023-10-27 16:34:47 -0300

    segunda pre entrega

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -3,7 +3,7 @@
 
 const deleteFromCart = async (pid) => {
   try {
-    const cid = "65259c66629d0fc68ead263e";
+    const cid = "65295538c0fbd8486dd71250";
     if (!cid) {
       console.log("cart not found");
     }
@@ -14,17 +14,8 @@ const deleteFromCart = async (pid) => {
       }
     );
     if (response.status == 200) {
-      const productElement = document.getCartById(`productId-${pid}`);
-      if (productElement) {
-        productElement.remove();
-      }
-      Swal.fire({
-        text: "product deleted successfully",
-        toast: true,
-        position: "top",
-        showConfirmButton: false,
-        timer: 1000,
-      });
+      const productElement = document.getElementById(`productId-${pid}`);
+    console.log('El producto se elimino con exito', productElement)
     }
   } catch (error) {
     console.log("error deleting product from cart: ", error);
